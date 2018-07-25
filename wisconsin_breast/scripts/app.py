4a803e2cd4326edda49fbfafc76b0a8058c6c0b7
Bweestyle <bwee@andrew.cmu.edu>
2018-07-24 20:38:19 -0700

    modularize

--- wisconsin_breast/scripts/app.py
+++ wisconsin_breast/scripts/app.py
@@ -2,6 +2,7 @@ from ml_utils.run_models import (preprocess_data, train_predict_evaluate)
 from ml_utils.parse_data import parse_data
 
 
+
 def main():
     with open("../train.csv") as f:
         (train_X, train_y, train_header) = parse_data(f, 1, "M")
@@ -9,9 +10,11 @@ def main():
     with open("../val.csv") as f:
         (val_X, val_y, val_header) = parse_data(f, 1, "M")
 
-    train_predict_evaluate(
+    predicitons = train_predict_evaluate(
         train_X, val_X, train_y, val_y,
         ["LR", "SVC", "DT", "ADA"])
+
+
 
     print("DONE")
 
